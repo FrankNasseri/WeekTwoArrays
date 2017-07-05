7740732ae6f01161733ea90635096d8a984b0019
Frank Nasseri <franknasseri57@gmail.com>
2017-07-05 10:17:35 -0400

    practicing arrays

--- WeekTwoArrays/Program.cs
+++ WeekTwoArrays/Program.cs
@@ -10,6 +10,16 @@ namespace WeekTwoArrays
     {
         static void Main(string[] args)
         {
+            //declaring an array without initializing
+            int[] myFirstArray;
+            //declaring and initializing a second array
+            string[] myStringArray = { "First", "Second", "Third", "Fourth" };
+            //declare and initialize a char array with the letters of your last name
+            char[] myName = { 'N', 'A', 'S', 'S', 'E', 'R', 'I' };
+            //I want my console to print three elements of my array 
+            Console.WriteLine(myName[0]);
+            Console.WriteLine(myName[3]);
+            Console.WriteLine(myName[1]);
         }
     }
 }
