865b0e25462e3e6d8d0851f6cf1efd337bca7d57
Frank Nasseri <franknasseri57@gmail.com>
2017-07-05 11:09:08 -0400

    more practice with arrays

--- WeekTwoArrays/Program.cs
+++ WeekTwoArrays/Program.cs
@@ -10,16 +10,45 @@ namespace WeekTwoArrays
     {
         static void Main(string[] args)
         {
-            //declaring an array without initializing
-            int[] myFirstArray;
-            //declaring and initializing a second array
-            string[] myStringArray = { "First", "Second", "Third", "Fourth" };
-            //declare and initialize a char array with the letters of your last name
-            char[] myName = { 'N', 'A', 'S', 'S', 'E', 'R', 'I' };
-            //I want my console to print three elements of my array 
-            Console.WriteLine(myName[0]);
-            Console.WriteLine(myName[3]);
-            Console.WriteLine(myName[1]);
+            ////declaring an array without initializing
+            //int[] myFirstArray;
+            ////declaring and initializing a second array
+            //string[] myStringArray = { "First", "Second", "Third", "Fourth" };
+            ////declare and initialize a char array with the letters of your last name
+            //char[] myName = { 'N', 'A', 'S', 'S', 'E', 'R', 'I' };
+            ////I want my console to print three elements of my array 
+            //Console.WriteLine(myName[0]);
+            //Console.WriteLine(myName[3]);
+            //Console.WriteLine(myName[1]);
+            //Create the following arrays and print using the correct index numbers:
+            //Array of Top 10 vacations spots
+            //string[] vacationSpots = { "Austarlia", "Dubai", "Hawaii", "Iceland", "Cleveland", "Los Angeles", "San Diego", "Aruba", "Jamaica", "Cancun" };
+            ////Print the frist and last vacations
+            //Console.WriteLine(vacationSpots[0]);
+            //Console.WriteLine(vacationSpots[9]);
+            //Array of the ages of 3 children you know
+            //int[] childAge = { 5, 10, 2 };
+            ////all childrens age on one line
+            //Console.WriteLine(childAge[0] + " " + childAge[1] + " " + childAge[2]);
+            ////print each childs age
+            //Console.WriteLine(childAge[0]);
+            //Console.WriteLine(childAge[1]);
+            //Console.WriteLine(childAge[2]);
+            ////Array of GPA's on a 4.0 scale of 8 students - you dont have to print
+            //double[] yourGpa = { 1.0, 1.5, 2.0, 2.5, 3.0, 3.2, 3.8, 4.0 };
+            //Array of First Letter of 4 of your classmates names who are sitting near you
+            //char[] className = { 'J', 'M', 'C', 'F' };
+            ////print all four on the same line JMCF
+            //Console.WriteLine(className);
+            ////print on different lines which prints as JMCF
+            //Console.Write(className[0]);
+            //Console.Write(className[1]);
+            //Console.Write(className[2]);
+            //Console.Write(className[3]);
+            ////different way of printing on one line, which prints as J, M, C, F
+            //Console.WriteLine("\n{0}, {1}, {2}, {3}", className[0], className[1], className[2], className[3]);
+
+
         }
     }
 }
